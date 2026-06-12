35015b437abf48be29c1cd6083d6f9495662fb22
KAYA <harveykaya1@gmail.com>
2026-06-12 19:15:09 +0200

    Delete system directory

--- system/utils.js
+++
@@ -1,8 +0,0 @@
-// system/utils.js
-export function formatUptime(ms) {
-  const s = Math.floor(ms / 1000) % 60;
-  const m = Math.floor(ms / (1000 * 60)) % 60;
-  const h = Math.floor(ms / (1000 * 60 * 60)) % 24;
-  const d = Math.floor(ms / (1000 * 60 * 60 * 24));
-  return `${d}d ${h}h ${m}m ${s}s`;
-}
